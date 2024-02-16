51f3c679a15a3c56cd425ab13eb22a73898e4519
Paul Gathondu <aulgondu@gmail.com>
2024-02-16 15:22:51 -0800

    feat(client): chart setup, create the 1 hour 5 min avg price chart

--- nextjs/src/app/dashboard/components/overview.tsx
+++ nextjs/src/app/dashboard/components/overview.tsx
@@ -1,36 +1,69 @@
-"use client";
-
-import usePriceData from "@/hooks/use-price-data";
+import React from "react";
 import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import usePriceData from "@/hooks/use-price-data";
+import { Price } from "@/types/data";
+
+type ProcessedPriceData = {
+  timestamp: number;
+  amount: string;
+};
 
 export function Overview() {
   const socketUrl = "ws://localhost:8000/ws/coinbase/prices";
-
   const priceData = usePriceData(socketUrl, "BTC");
+
+  // Preprocess the data
+  const processData = (data: Price[]): ProcessedPriceData[] => {
+    const newData: ProcessedPriceData[] = [];
+    const intervals: Record<number, { timestamp: number; amount: number[] }> =
+      {};
+
+    // Group data into 5-minute intervals
+    data.forEach((item) => {
+      const timestamp = Math.floor(item.timestamp / 300) * 300; // Round to 5-minute interval
+      if (!intervals[timestamp]) {
+        intervals[timestamp] = {
+          timestamp,
+          amount: [],
+        };
+      }
+      intervals[timestamp].amount.push(parseFloat(item.amount));
+    });
+
+    // Calculate average price for each interval
+    for (const timestamp in intervals) {
+      const averagePrice =
+        intervals[timestamp].amount.reduce((a, b) => a + b, 0) /
+        intervals[timestamp].amount.length;
+      newData.push({
+        timestamp: parseInt(timestamp),
+        amount: averagePrice.toFixed(2),
+      });
+    }
+
+    return newData;
+  };
+
+  const processedData = processData(priceData);
 
   return (
     <ResponsiveContainer width="100%" height={350}>
-      <LineChart data={priceData}>
+      <LineChart data={processedData}>
         <XAxis
           dataKey="timestamp"
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
+          type="number"
+          domain={["dataMin", "dataMax"]}
           tickFormatter={(timestamp) => {
-            // Convert timestamp to milliseconds
-            const milliseconds = timestamp * 1000;
-            // Create a new Date object with the milliseconds
-            const dateObject = new Date(milliseconds);
-            // Format the date as desired
-            return dateObject.toLocaleString();
+            const dateObject = new Date(timestamp * 1000);
+            return dateObject.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            });
           }}
         />
         <YAxis
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
+          type="number"
+          domain={["auto", "auto"]}
           tickFormatter={(value) => `$${value}`}
         />
         <Line
